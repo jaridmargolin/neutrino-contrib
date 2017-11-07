7ed3f22ba5bdd95a350c0d25879d1b8c8e55b61c
Tim Kelty <tim@fusionary.com>
2017-11-07 09:55:38 -0500

    - Add css-hot-loader support - Clarify options - Change default file name to not include hash (as css-hot-loader will not work)

--- packages/neutrino-middleware-extractstyles/src/index.js
+++ packages/neutrino-middleware-extractstyles/src/index.js
@@ -19,21 +19,38 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
     options: styleRule.use('style').get('options'),
   }
 
+  options = Object.assign({
+    hot: true,
+    cssHotLoader: {},
+    extract: {},
+    plugin: {},
+    fallback: null,
+    use: null,
+  }, options);
+
   const styleLoaders = Array.from(styleRule.uses.store.keys())
     .filter((key) => key !== 'style')
     .map((key) => styleRule.use(key))
     .map((use) => ({loader: use.get('loader'), options: use.get('options')}))
 
-  const loaderOptions = Object.assign({
+  const extractOptions = Object.assign({
     fallback: options.fallback || styleFallback || 'style-loader',
     use: options.use || styleLoaders || 'css-loader',
-  }, options.loader || {})
+  }, options.extract)
 
   const pluginOptions = Object.assign({
-    filename: '[name].[hash].css',
-  }, options.plugin || {})
+    filename: '[name].css',
+  }, options.plugin)
 
-  const loaders = ExtractTextPlugin.extract(loaderOptions)
+  const loaders = ExtractTextPlugin.extract(extractOptions)
+
+  // css-hot-loader must be first
+  if (options.hot) {
+    loaders.unshift({
+      loader: 'css-hot-loader',
+      options: options.cssHotLoader,
+    })
+  }
 
   styleRule.uses.clear()
 
